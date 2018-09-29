034c69d42e1d358650f245cf514e1751212b40b9
Brian <Brian@ivypro.com>
2018-09-29 14:07:16 -0700

    Merge PgS-M

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -14,34 +14,57 @@ export class DataService {
   photoes = [];
   selectedEvent: any;
   currentImgUrl: string;
+  isApi:boolean= false;
 
   constructor(private http: HttpClient) { }
 
   //===============to get data once only==============
   getEventsPosts(location) {
-   // this.http.get('https://my-json-server.typicode.com/ivypro/playguide-angular/posts').subscribe(
-     // this.http.get('http://localhost:3000/posts').subscribe(
-    this.http.get("/assets/db.json").subscribe(
-      (res: any) => {
-        this.events = [];
-        this.photoes = [];
-        // res= this.shortByDate(res);
-        res.posts= this.shortByDate(res.posts);
-         // res.forEach(e => {
-        res.posts.forEach(e => {
-          if (e.location.replace(" ", "") == location) {
-            this.events.push(e);
-            this.photoes.push(e.images[0])
+   if(this.isApi){
+    this.http.get('https://my-json-server.typicode.com/ivypro/playguide-angular/posts').subscribe(
+        (res: any) => {
+          this.events = [];
+          this.photoes = [];
+          res = this.filterByCurrentDate(res);
+          res= this.shortByDate(res);
+          res.forEach(e => {
+            if (e.location.replace(" ", "") == location) {
+              this.events.push(e);
+              this.photoes.push(e.images[0])
+            }
+          });
+         console.log('res',res);
+          if(this.events[0]){
+            this.currentImgUrl = this.events[0].images[0];
+            this.selectedEvent=this.events[0];
           }
-        });
-       console.log('res',res);
-        if(this.events[0]){
-          this.currentImgUrl = this.events[0].images[0];
-          this.selectedEvent=this.events[0];
+          return this.events
         }
-        return this.events
-      }
-    )
+      )
+   }
+   else{
+
+      this.http.get("/assets/db.json").subscribe(
+        (res: any) => {
+          this.events = [];
+          this.photoes = [];
+          res.posts = this.filterByCurrentDate(res.posts);
+          res.posts= this.shortByDate(res.posts);
+          res.posts.forEach(e => {
+            if (e.location.replace(" ", "") == location) {
+              this.events.push(e);
+              this.photoes.push(e.images[0])
+            }
+          });
+         console.log('res',res);
+          if(this.events[0]){
+            this.currentImgUrl = this.events[0].images[0];
+            this.selectedEvent=this.events[0];
+          }
+          return this.events
+        }
+      )
+   }
   }
 
   selectedItem(event) {
@@ -51,17 +74,32 @@ export class DataService {
   }
 
   shortByDate(items:any){
-    return items.sort((a: any, b: any) =>
-    new Date(a.date).getTime() - new Date(b.date).getTime()
-);
+    //return items.sort((a: any, b: any) =>  new Date(a.date).getTime() - new Date(b.date).getTime());
+	   return items.sort(function (a: any, b: any) {
+      var l=this.dateConverter(a.date).getTime();
+      var r=this.dateConverter(b.date).getTime();
+      return (l < r) ? -1 : ((l > r) ? 1 : 0);
+     }.bind(this));
+  }
+
+filterByCurrentDate(items: any) {
+    var yesterday=new Date();
+      yesterday.setDate(yesterday.getDate()-1)
+    console.log(yesterday);
+    return items.filter(a => (this.dateConverter(a.date).getTime() - yesterday.getTime()) > 0)
+  }
+
+
+  dateConverter(a) {
+    var splittedDate = a.split("-");
+
+    return new Date(splittedDate[2], Number(splittedDate[0]) - 1, splittedDate[1]);
   }
 
 
 
 
-
-
-
+// Precusor for [e] typescrip // MATCH URL ABOVE (getEventsPosts)
 
   getPosts() {
     return this.http.get('https://my-json-server.typicode.com/ivypro/playguide-angular/posts')
